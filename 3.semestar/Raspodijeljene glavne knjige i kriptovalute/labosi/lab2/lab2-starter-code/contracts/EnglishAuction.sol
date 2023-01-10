b3238fbdff5c39a97913fe9937ddb7a3ee2153d3
eduardduras <eduard.duras@axilis.com>
2023-01-10 09:18:12 +0100

    asdas

--- 3.semestar/Raspodijeljene glavne knjige i kriptovalute/labosi/lab2/lab2-starter-code/contracts/EnglishAuction.sol
+++ 3.semestar/Raspodijeljene glavne knjige i kriptovalute/labosi/lab2/lab2-starter-code/contracts/EnglishAuction.sol
@@ -4,12 +4,11 @@ pragma solidity ^0.8.0;
 import "./Auction.sol";
 
 contract EnglishAuction is Auction {
-
-    uint internal highestBid;
-    uint internal initialPrice;
-    uint internal biddingPeriod;
-    uint internal lastBidTimestamp;
-    uint internal minimumPriceIncrement;
+    uint256 internal highestBid;
+    uint256 internal initialPrice;
+    uint256 internal biddingPeriod;
+    uint256 internal lastBidTimestamp;
+    uint256 internal minimumPriceIncrement;
 
     address internal highestBidder;
 
@@ -17,9 +16,9 @@ contract EnglishAuction is Auction {
         address _sellerAddress,
         address _judgeAddress,
         Timer _timer,
-        uint _initialPrice,
-        uint _biddingPeriod,
-        uint _minimumPriceIncrement
+        uint256 _initialPrice,
+        uint256 _biddingPeriod,
+        uint256 _minimumPriceIncrement
     ) Auction(_sellerAddress, _judgeAddress, _timer) {
         initialPrice = _initialPrice;
         biddingPeriod = _biddingPeriod;
@@ -30,21 +29,37 @@ contract EnglishAuction is Auction {
     }
 
     function bid() public payable {
-        require(time() - lastBidTimestamp < biddingPeriod);
-        require(msg.value >= initialPrice && 
-                msg.value > highestBid && 
-                msg.value - highestBid >= minimumPriceIncrement
-        );
+        uint256 cijena = 0;
+        if (highestBid != 0 && highestBidder != address(0)) {
+            cijena = highestBid + minimumPriceIncrement;
+        } else {
+            cijena = initialPrice;
+        }
+        require(msg.value >= cijena);
+        require((time() - lastBidTimestamp) < biddingPeriod);
 
-        payable(highestBidder).transfer(highestBid);
+        bool check_if = false;
+        address prethodni_najveci_ulagac = address(0);
+        uint256 prethodna_najvisa_ponuda = 0;
+        if (highestBid != 0 && highestBidder != address(0)) {
+            prethodni_najveci_ulagac = highestBidder;
+            prethodna_najvisa_ponuda = address(this).balance - msg.value;
+            check_if = true;
+        }
 
-        highestBid = msg.value;
-        lastBidTimestamp = block.timestamp;
         highestBidder = msg.sender;
+        highestBid = msg.value;
+        lastBidTimestamp = time();
+
+        if (check_if) {
+            payable(prethodni_najveci_ulagac).transfer(
+                prethodna_najvisa_ponuda
+            );
+        }
     }
 
-    function getHighestBidder() override public view returns (address) {
-        if(time() - lastBidTimestamp < biddingPeriod){
+    function getHighestBidder() public view override returns (address) {
+        if (time() - lastBidTimestamp < biddingPeriod) {
             return address(0);
         }
 
@@ -54,5 +69,4 @@ contract EnglishAuction is Auction {
     function enableRefunds() public {
         outcome = Outcome.NOT_SUCCESSFUL;
     }
-
 }
