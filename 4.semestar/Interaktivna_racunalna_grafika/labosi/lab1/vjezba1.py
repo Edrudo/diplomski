5ca14fd021bc809db3ade6a79eee6be7e2d8f30c
eduardduras <eduard.duras@axilis.com>
2023-03-12 17:59:44 +0100

    wip

---
+++ 4.semestar/Interaktivna_racunalna_grafika/labosi/lab1/vjezba1.py
@@ -0,0 +1,93 @@
+from ipaddress import v4_int_to_packed
+import numpy as np
+
+if __name__ == "__main__":
+    print("Zadatak 1")
+
+    v1=np.array([2, 3, -4]) + np.array([-1, 4, -1])
+    print(f'{v1=}')
+
+    s=np.dot(v1,np.array([-1, 4, -1]))
+    print(f'{s=}')
+
+    v2=np.cross(v1,np.array([2, 2, 4]))
+    print(f'{v2=}')
+
+    v3=np.linalg.norm(v2)
+    print(f'{v3=}')
+
+    v4=-v2
+    print(f'{v4=}')
+
+    Mx=np.array([
+        [1,2,3], 
+        [2,1,3], 
+        [4,5,1]
+    ])
+    My=np.array([
+        [-1,2,-3], 
+        [5,-2,7], 
+        [-4,-1,3]
+    ])
+    
+    M1=np.add(Mx,My) # zbrajanje matrica
+    print(f'{M1=}')
+    
+    M2=np.dot(Mx,np.transpose(My)) # mnozenje matrica
+    print(f'{M2=}')
+
+    M3=np.dot(Mx,np.linalg.inv(My))
+    print(f'{M3=}')
+
+
+    print("Zadatak 2")
+
+    A=np.array([
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ])
+    B=np.array([0,0,0])
+    j=0
+    
+    for i in range(0,9):
+        i1=i%3
+        A[j][i1]=input("Upisite broj A:\n")
+        print(A)   
+        
+        if i%3==2: 
+            B[j]=input("Upisite broj B:\n")
+            print(B)
+            j=j+1
+
+    print("\n\n\nRjesenja x, y, z = ", np.linalg.inv(A).dot(B))
+
+    print('Zadatak 3:')
+
+    A=[0,0,0]
+    B=[0,0,0]
+    C=[0,0,0]
+    T=[0,0,0]
+
+    A[0]=int(input("x tocke A:"))
+    A[1]=int(input("z tocke A:"))
+    A[2]=int(input("y tocke A:"))
+
+    B[0]=int(input("x tocke B:"))
+    B[1]=int(input("z tocke B:"))
+    B[2]=int(input("y tocke B:"))
+
+    C[0]=int(input("x tocke C:"))
+    C[1]=int(input("z tocke C:"))
+    C[2]=int(input("y tocke C:"))
+
+    T[0]=int(input("x tocke T:",))
+    T[1]=int(input("z tocke T:"))
+    T[2]=int(input("y tocke T:"))
+
+    M=np.transpose(np.array([A,B,C]))
+    print(M)
+    Minv=np.linalg.inv(np.array(M))
+    
+    t=np.dot(Minv, T)
+    print(t)
---
+++ 4.semestar/Interaktivna_racunalna_grafika/labosi/lab1/vjezba2.py
@@ -0,0 +1,72 @@
+import pyglet
+from pyglet.gl import *
+from pyglet import shapes
+
+
+def bresenham_line(x0, y0, x1, y1):
+    points = []
+    dx = abs(x1 - x0)
+    dy = abs(y1 - y0)
+    x, y = x0, y0
+    sx = -1 if x0 > x1 else 1
+    sy = -1 if y0 > y1 else 1
+    if dx > dy:
+        err = dx / 2.0
+        while x != x1:
+            points.append((x, y))
+            err -= dy
+            if err < 0:
+                y += sy
+                err += dx
+            x += sx
+    else:
+        err = dy / 2.0
+        while y != y1:
+            points.append((x, y))
+            err -= dx
+            if err < 0:
+                x += sx
+                err += dy
+            y += sy
+    points.append((x, y))
+    return points
+
+point1=(0,0)
+point2=(0,0)
+drawingInProgress=False
+linePoints=[]
+    
+def main():
+    window = pyglet.window.Window()
+    batch = pyglet.graphics.Batch()
+    
+    @window.event
+    def on_mouse_press(x, y, button, modifiers):
+        global drawingInProgress, point1, point2, linePoints
+        
+        if button:
+            if not drawingInProgress:
+                point1=(x, y)
+                drawingInProgress = True
+            else:
+                point2=(x, y)
+                linePoints=bresenham_line(point1[0], point1[1], x, y)
+                drawingInProgress=False
+                
+                    
+    @window.event                        
+    def on_draw():
+        global drawingInProgress, linePoints, point1, point2
+        
+        if(not drawingInProgress):
+            line=shapes.Line(point1[0], point1[1] + 20, point2[0], point2[1] + 20, width=3, batch=batch)
+            for point in linePoints:
+                pyglet.graphics.draw(1, pyglet.gl.GL_POINTS,
+                          ('v2i', point),
+                          ('c3B', (255, 255, 255)))
+            batch.draw()
+            
+    pyglet.app.run()
+    
+if __name__ == "__main__":
+    main()
