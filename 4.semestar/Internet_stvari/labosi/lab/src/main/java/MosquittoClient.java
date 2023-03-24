c3fea82fc890e2196892e340dde7eec6e5d50dad
eduard.duras <eduard.duras@gmail.com>
2023-03-24 12:53:44 +0100

    sdsd

---
+++ 4.semestar/Internet_stvari/labosi/lab/src/main/java/MosquittoClient.java
@@ -0,0 +1,34 @@
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MosquittoClient {
+    String topic        = "MQTT Examples";
+    String content      = "Message from MqttPublishSample";
+    int qos             = 2;
+    String broker       = "tcp://mqtt.eclipseprojects.io:1883";
+    String clientId     = "JavaSample";
+    MemoryPersistence persistence = new MemoryPersistence();
+    MqttClient sampleClient;
+
+    public MosquittoClient() {
+        try {
+            // mqtt client init and broker connection
+            this.sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: " + broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+
+        } catch (MqttException me) {
+            System.out.println("reason " + me.getReasonCode());
+            System.out.println("msg " + me.getMessage());
+            System.out.println("loc " + me.getLocalizedMessage());
+            System.out.println("cause " + me.getCause());
+            System.out.println("excep " + me);
+            me.printStackTrace();
+        }
+    }
+}
