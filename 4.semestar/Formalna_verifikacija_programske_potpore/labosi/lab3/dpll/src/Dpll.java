e796e5bb3a1fbc888ab2a05a72e434d76a985357
eduardduras <eduard.duras@axilis.com>
2023-06-01 21:19:58 +0200

    Refactored dpll

--- 4.semestar/Formalna_verifikacija_programske_potpore/labosi/lab3/dpll/src/Dpll.java
+++ 4.semestar/Formalna_verifikacija_programske_potpore/labosi/lab3/dpll/src/Dpll.java
@@ -1,110 +1,65 @@
-/*
-  Simple Java implementation of the Davis–Putnam–Logemann–Loveland (DPLL) algorithm
-  ( https://en.wikipedia.org/wiki/DPLL_algorithm )
-  including OLR (one-literal-rule) and PLR (pure-literal-rule)
-
-  Checks whether a given formula is satisfiable or not
-
-  https://github.com/yoshc
-  18.2.2018
-
-  examples:
-    Checking if the formula
-      (¬p ∨ q ∨ ¬r ∨ s) ∧ (¬q ∨ ¬r ∨ s) ∧ (r) ∧ (¬p ∨ ¬s) ∧ (¬p ∨ r)
-    is satisfiable can be done by using
-      String[][] formula = {{"-p", "q", "-r", "s"}, {"-q", "-r", "s"}, {"r"}, {"-p", "-s"}, {"-p", "r"}};
-      DpllResult result = Dpll.solve(new Formula(formula));
-
-    Disabling the output can be done by using
-      Dpll.disableLog()
-
-    Note that '-' is used instead of ¬
-
- */
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Dpll {
-
-  private static boolean logEnabled = true;
-
-  /**
-   * Solves a given formula using the DPLL algorithm
-   * @param formula the formula
-   * @return the Result of the solved formula
-   */
   public static DpllResult solve(Formula formula) {
-    log(0, "Formula CNF: " + formula.toStringCnf());
-    final long timeNow = System.currentTimeMillis();
+    System.out.println("CNF formula: " + formula.toStringCnf());
+    final long timeStart = System.currentTimeMillis();
     HashMap<String, Boolean> settings = new HashMap<>();
-    int orderSize = formula.getAllLiterals().size();
-    DpllResult result = solve(formula, settings, orderSize, 0);
+    DpllResult result = solve(formula, settings, 0);
     if(result != null) {
-      result.setTimeTaken(System.currentTimeMillis()-timeNow);
+      result.setTimeTaken(System.currentTimeMillis()-timeStart);
     }
     return result;
   }
 
-  /**
-   * internal recursive method for the DPLL algorithm
-   * @param formula
-   * @param variableSetting
-   * @param orderSize
-   * @param tabs
-   * @return
-   */
-  private static DpllResult solve(Formula formula, HashMap<String, Boolean> variableSetting, int orderSize, int tabs) {
-    log(tabs,"Solving formula with " + formula.getClauses().size() + " clauses and " + formula.getAllLiterals().size() + " literals:");
-    log(tabs,"As set of clauses: \t" + formula.toString());
-
-    // {}
+  private static DpllResult solve(Formula formula, HashMap<String, Boolean> variableSetting, int depth) {
     if(formula.getClauses().size() == 0) {
       return new DpllResult(variableSetting);
     }
 
-    // {{}}
     if(formula.isOnlyEmptyClauses()) {
-      log(tabs,"Only empty clauses! Going up");
+      // log(depth,"Only empty clauses! Going up");
       return null;
     }
 
     ArrayList<String> literals = formula.getAllLiterals();
 
-    // First: Check if OLR is usable
-    int rule = 0; // 0 = none, 1 = left/true, 2 = right/false
+    // First: Check if UCP is usable
     String lit = null;
-    olrLoop:for(Clause clause : formula.getClauses()) {
+    boolean litValue = false;
+    boolean ucpFound = false;
+    for(Clause clause : formula.getClauses()) {
       if(clause.getLiterals().size() == 1) {
+        ucpFound = true;
         lit = clause.getLiterals().get(0);
         if(lit.startsWith("-")) {
           lit = lit.replaceFirst("-","");
-          rule = 2;
         } else {
-          rule = 1;
+          litValue = true;
         }
-        log(tabs, "OLR: " + lit + " => " + (rule == 1 ? "true" : "false"));
-        break olrLoop;
+        // log(depth, "UCP: " + lit + " => " + (rule == 1 ? "true" : "false"));
+        break;
       }
     }
 
-    if(rule == 0) {
-      // Only look for PLR if OLR is not usable
-      // since OLR has a higher priority than OLR
-      plrLoop:for(Clause c : formula.getClauses()) {
+    boolean pleFound = false;
+    if(!ucpFound) {
+      // Only look for PLE if UCP is not usable
+      // since UCP has a higher priority than UCP
+      for(Clause c : formula.getClauses()) {
         for(String literal : c.getLiterals()) {
-          String opposite = literal.startsWith("-") ? literal.replaceFirst("-","") : "-" + literal;
-          if(!formula.containsLiteral(opposite)) {
-            // found PLR
+          String oppositeLiteral = literal.startsWith("-") ? literal.replaceFirst("-","") : "-" + literal;
+          if(!formula.containsLiteral(oppositeLiteral)) {
+            pleFound = true;
             if(literal.startsWith("-")) {
               lit = literal.replaceFirst("-","");
-              rule = 2;
             } else {
+              litValue = true;
               lit = literal;
-              rule = 1;
             }
-            log(tabs, "PLR: " + lit + " => " + (rule == 1 ? "true" : "false"));
-            break plrLoop;
+            // log(depth, "PLE: " + lit + " => " + litValue);
+            break;
           }
         }
       }
@@ -114,67 +69,54 @@ public class Dpll {
       lit = literals.get(0);
     }
 
-    // Don't do this if OLR result was 2
+    // Don't do this if UCP result was 2
     // since that would mean we skip the true case and set x := false
-    if(rule == 0 || rule == 1) {
+    if((!ucpFound && !pleFound) || litValue) {
       // Left part ( x:= true)
-      Formula newFormulaL = Formula.copy(formula);
-      HashMap<String, Boolean> newSettingsL = (HashMap<String, Boolean>) variableSetting.clone();
+      Formula newFormulaLeft = Formula.copy(formula);
+      HashMap<String, Boolean> newSettingsLeft = new HashMap<>(variableSetting);
 
-      StringBuffer sb = new StringBuffer();
-      sb.append("[L] " + lit + ":= true (");
-      for (String key : newSettingsL.keySet()) {
-        sb.append(" " + key + "=" + newSettingsL.get(key));
+      StringBuilder sb = new StringBuilder();
+      sb.append("[L] ").append(lit).append(":= true (");
+      for (String key : newSettingsLeft.keySet()) {
+        sb.append(" ").append(key).append("=").append(newSettingsLeft.get(key));
       }
       sb.append(" )");
-      log(tabs, sb.toString());
-      newSettingsL.put(lit, true);
-      newFormulaL.set(lit, true);
+      log(depth, sb.toString());
+      newSettingsLeft.put(lit, true);
+      newFormulaLeft.set(lit, true);
 
-      DpllResult resultL = solve(newFormulaL, newSettingsL, orderSize, tabs + 1);
+      DpllResult resultL = solve(newFormulaLeft, newSettingsLeft, depth + 1);
 
       if (resultL != null) {
         return resultL;
       }
     }
-    // Don't do this if OLR result was 1
+
+    // Don't do this if UCP result was 1
     // since that would mean we skip the false case and set x := true
-    if(rule == 0 || rule == 2) {
+    if((!ucpFound && !pleFound) || !litValue) {
       // Right part ( x:= false )
-      Formula newFormulaR = Formula.copy(formula);
-      HashMap<String, Boolean> newSettingsR = (HashMap<String, Boolean>) variableSetting.clone();
+      Formula newFormulaRight = Formula.copy(formula);
+      HashMap<String, Boolean> newSettingsR = new HashMap<>(variableSetting);
 
       StringBuilder sb = new StringBuilder();
-      sb.append("[R] " + lit + ":= false (");
+      sb.append("[R] ").append(lit).append(":= false (");
       for (String key : newSettingsR.keySet()) {
-        sb.append(" " + key + "=" + newSettingsR.get(key));
+        sb.append(" ").append(key).append("=").append(newSettingsR.get(key));
       }
       sb.append(" )");
-      log(tabs, sb.toString());
+      log(depth, sb.toString());
       newSettingsR.put(lit, false);
-      newFormulaR.set(lit, false);
+      newFormulaRight.set(lit, false);
 
-      DpllResult resultR = solve(newFormulaR, newSettingsR, orderSize, tabs + 1);
-      if (resultR != null) {
-        return resultR;
-      }
+      return solve(newFormulaRight, newSettingsR, depth + 1);
     }
 
     return null;
   }
 
-  public static void disableLog() {
-    Dpll.logEnabled = false;
-  }
-
-  public static void enableLog() {
-    Dpll.logEnabled = true;
-  }
-
   private static void log(int tabs, String msg) {
-    if(!logEnabled) {
-      return;
-    }
     for(int i = 0; i < tabs; i++) {
       System.out.print("\t");
     }
