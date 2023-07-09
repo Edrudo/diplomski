79b3e9e2ae62d5499c78b75cb9d1d01961dba838
eduardduras <eduard.duras@axilis.com>
2023-07-09 16:35:58 +0200

    vjezba6

--- 4.semestar/Interaktivna_racunalna_grafika/labosi/ponovo/lab3/vjezba6/vjezba6.py
+++ 4.semestar/Interaktivna_racunalna_grafika/labosi/ponovo/lab3/vjezba6/vjezba6.py
@@ -4,26 +4,34 @@ import pyglet
 import numpy as np
 from transformationMatrixes import *
 import pyglet.window.key as key
+from time import sleep
 
 
-window = pyglet.window.Window()
+window = pyglet.window.Window(
+    1280, 720, config=pyglet.gl.Config(double_buffer=True))
 window.projection = pyglet.window.Projection3D()
 
 
 vertices = []
 verticesTransformed = []
+polygonsVisible = []
 polygons = []
 O, G, V = [], [], []
+planeCoefs = []
+controlPoints = []
+bezierCurvePoints = []
+objectCenter = []
+animation = False
 
 T, P = [], []
 
 xaxis, yaxis, zaxis = [], [], []
 
 
-def loadData(filename):
-    global vertices, polygons, O, G, V
+def loadData(filenameObj, filenameControlPolygon):
+    global vertices, polygons, O, G, V, objectCenter
 
-    with open(filename) as f:
+    with open(filenameObj) as f:
         for line in f.readlines():
             if line.startswith("v"):
                 v = line.split(" ")
@@ -54,6 +62,8 @@ def loadData(filename):
     ycenter = (ymin + ymax) / 2
     zcenter = (zmin + zmax) / 2
 
+    objectCenter = [xcenter, ycenter, zcenter]
+
     for v in vertices:
         v[0] -= xcenter
         v[1] -= ycenter
@@ -66,6 +76,14 @@ def loadData(filename):
                        v[1] / maxdimension,
                        v[2] / maxdimension,
                        1]
+
+    with open(filenameControlPolygon) as f:
+        for line in f.readlines():
+            coords = line.split(",")
+            controlPoints.append(
+                [float(coords[0]), float(coords[1]), float(coords[2])])
+
+    caluculateBezierCurvePoints()
 
 
 def calculateAxisVUp():
@@ -116,7 +134,7 @@ def calculateTransformationMatrixWithTransformations():
 
 
 def calculatePerspectiveMatrix():
-    global P
+    global P, O, G
 
     z = np.subtract(G, O)
     zaxis = z / np.linalg.norm(z)
@@ -144,6 +162,8 @@ def transformVertices():
 
 @window.event
 def on_key_press(symbol, modifiers):
+    global animation
+
     if symbol == key.A:
         O[0] -= 1
     if symbol == key.D:
@@ -167,33 +187,124 @@ def on_key_press(symbol, modifiers):
         G[1] += 1
 
     transformVertices()
+    findVisiblePolygons()
+
+    if symbol == key.SPACE:
+        animation = True
+
+
+def calculatePlaneCoefs():
+    global planeCoefs, vertices, polygons
+
+    for p in polygons:
+        v1 = vertices[p[0]]
+        v2 = vertices[p[1]]
+        v3 = vertices[p[2]]
+
+        A = (v2[1] - v1[1]) * (v3[2] - v1[2]) - \
+            (v2[2] - v1[2]) * (v3[1] - v1[1])
+        B = -(v2[0] - v1[0]) * (v3[2] - v1[2]) + \
+            (v2[2] - v1[2]) * (v3[0] - v1[0])
+        C = (v2[0] - v1[0]) * (v3[1] - v1[1]) - \
+            (v2[1] - v1[1]) * (v3[0] - v1[0])
+        D = -v1[0] * A - v1[1] * B - v1[2] * C
+
+        planeCoefs.append([A, B, C, D])
+
+
+def findVisiblePolygons():
+    global vertices, polygons, polygonsVisible, planeCoefs, O
+    calculatePlaneCoefs()
+
+    polygonsVisible = []
+
+    for i, p in enumerate(polygons):
+        v1 = vertices[p[0]]
+        v2 = vertices[p[1]]
+        v3 = vertices[p[2]]
+        centerPoint = [float((v1[0] + v2[0] + v3[0]) / 3),
+                       float((v1[1] + v2[1] + v3[1]) / 3),
+                       float((v1[2] + v2[2] + v3[2]) / 3)]
+
+        OcenterPointVector = np.subtract(centerPoint, O)
+
+        if np.dot([planeCoefs[i][0], planeCoefs[i][1], planeCoefs[i][2]], OcenterPointVector) >= 0:
+            # if np.dot([planeCoefs[i][0], planeCoefs[i][1], planeCoefs[i][2]], O) + planeCoefs[i][3] > 0:
+
+            polygonsVisible.append(p)
+
+
+def caluculateBezierCurvePoints():
+    global bezierCurvePoints
+
+    n = len(controlPoints)
+
+    for t1 in range(0, 101):
+        t = float(t1 / 100)
+
+        curveCoords = [0, 0, 0]
+        for i, controlPoint in enumerate(controlPoints):
+            curveCoords += np.multiply(((np.math.factorial(n) / (np.math.factorial(i) *
+                                                                 np.math.factorial(n-i))) * t**i * (1-t)**(n-i)), controlPoint)
+
+        bezierCurvePoints.append(curveCoords)
 
 
 @window.event
 def on_draw():
-    global verticesTransformed, polygons
+    global verticesTransformed, polygonsVisible, animation
 
-    glClear(GL_COLOR_BUFFER_BIT)
-    glMatrixMode(GL_MODELVIEW)
+    if animation:
+        global O, G, objectCenter
 
-    glBegin(GL_TRIANGLES)
-    for p in polygons:
-        v1 = verticesTransformed[p[0]]
-        v2 = verticesTransformed[p[1]]
-        v3 = verticesTransformed[p[2]]
-        glColor3f(1, 1, 0)
-        glVertex3d(v1[0], v1[1], v1[2])
+        G = objectCenter
+
+        for bPoint in bezierCurvePoints:
+            O = bPoint
+            transformVertices()
+            findVisiblePolygons()
+
+            glClear(GL_COLOR_BUFFER_BIT)
+            glMatrixMode(GL_MODELVIEW)
+
+            glBegin(GL_LINE_STRIP)
+            for p in polygonsVisible:
+                v1 = verticesTransformed[p[0]]
+                v2 = verticesTransformed[p[1]]
+                v3 = verticesTransformed[p[2]]
+                glVertex3d(v1[0], v1[1], v1[2])
+                glVertex3d(v2[0], v2[1], v2[2])
+                glVertex3d(v3[0], v3[1], v3[2])
+            glEnd()
+
+        animation = False
+
+    else:
+        glClear(GL_COLOR_BUFFER_BIT)
+        glMatrixMode(GL_MODELVIEW)
+
+        glBegin(GL_LINE_STRIP)
+        for p in polygonsVisible:
+            v1 = verticesTransformed[p[0]]
+            v2 = verticesTransformed[p[1]]
+            v3 = verticesTransformed[p[2]]
+            glVertex3d(v1[0], v1[1], v1[2])
+            glVertex3d(v2[0], v2[1], v2[2])
+            glVertex3d(v3[0], v3[1], v3[2])
+        glEnd()
+
+        glMatrixMode(GL_MODELVIEW)
         glColor3f(1, 1, 1)
-        glVertex3d(v2[0], v2[1], v2[2])
-        glColor3f(0, 1, 1)
-        glVertex3d(v3[0], v3[1], v3[2])
-    glEnd()
+        glBegin(GL_POINTS)
+        for p in bezierCurvePoints:
+            glVertex3d(p[0], p[1], p[2])
+        glEnd()
 
 
 if __name__ == "__main__":
-    loadData(sys.argv[1])
-
+    loadData(sys.argv[1], sys.argv[2])
     transformVertices()
+    findVisiblePolygons()
 
     glTranslatef(0, 0, -3)
 
