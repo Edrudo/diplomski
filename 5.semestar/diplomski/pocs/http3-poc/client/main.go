920fba8979e59ce1d289d8c82187075e1ec64363
eduardduras <eduard.duras@axilis.com>
2023-12-26 18:20:34 +0100

    client v0

---
+++ 5.semestar/diplomski/pocs/http3-poc/client/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+
+	"github.com/quic-go/quic-go"
+	"github.com/quic-go/quic-go/http3"
+	"github.com/quic-go/quic-go/logging"
+	"github.com/quic-go/quic-go/qlog"
+
+	"http3-client-poc/internal/testdata"
+	"http3-client-poc/internal/utils"
+)
+
+type ImagePart struct {
+	ImageHash  string `json:"imageHash"`
+	PartNumber int    `json:"partNumber"`
+	TotalParts int    `json:"totalParts"`
+	PartData   []byte `json:"partData"`
+}
+
+func main() {
+	// extracting image path from args
+	args := os.Args
+	if len(args) < 3 {
+		log.Fatal(
+			errors.New(
+				"arguments needed for the program: " +
+					"\t - url where image will be sent at least one" +
+					"\t - path to image that needs to be sent",
+			),
+		)
+	}
+	addr := args[1]
+	imagePath := args[2]
+
+	logger := utils.DefaultLogger
+
+	image, err := os.ReadFile(imagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	imageParts := make([]ImagePart, 0)
+	numImageParts := len(image) / 1450
+	if len(imageParts)%1450 > 0 {
+		numImageParts++
+	}
+
+	roundTripper := initilizeRoundTripper()
+
+	defer roundTripper.Close()
+	hclient := &http.Client{
+		Transport: roundTripper,
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(numImageParts)
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < numImageParts; i++ {
+		bdy, err := json.Marshal(
+			ImagePart{
+				ImageHash:  "hash",
+				PartNumber: i + 1,
+				TotalParts: numImageParts,
+				PartData:   image[i*1450 : (i+1)*1450],
+			},
+		)
+		if err != nil {
+			panic(err)
+		}
+
+		logger.Infof("GET %s", addr)
+		rsp, err := hclient.Post(addr, "application/json", bytes.NewBuffer(bdy))
+		if err != nil {
+			log.Fatal(err)
+		}
+		logger.Infof("Got response for %s: %#v", addr, rsp)
+		wg.Done()
+	}
+	wg.Wait()
+}
+
+func initilizeRoundTripper() *http3.RoundTripper {
+	insecure := flag.Bool("insecure", true, "skip certificate verification")
+	enableQlog := flag.Bool("qlog", false, "output a qlog (in the same directory)")
+	flag.Parse()
+
+	pool, err := x509.SystemCertPool()
+	if err != nil {
+		log.Fatal(err)
+	}
+	testdata.AddRootCA(pool)
+
+	var qconf quic.Config
+	if *enableQlog {
+		qconf.Tracer = func(
+			ctx context.Context,
+			p logging.Perspective,
+			connID quic.ConnectionID,
+		) *logging.ConnectionTracer {
+			filename := fmt.Sprintf("client_%s.qlog", connID)
+			f, err := os.Create(filename)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("Creating qlog file %s.\n", filename)
+			return qlog.NewConnectionTracer(utils.NewBufferedWriteCloser(bufio.NewWriter(f), f), p, connID)
+		}
+	}
+
+	return &http3.RoundTripper{
+		TLSClientConfig: &tls.Config{
+			RootCAs:            pool,
+			InsecureSkipVerify: *insecure,
+		},
+		QuicConfig: &qconf,
+	}
+}
