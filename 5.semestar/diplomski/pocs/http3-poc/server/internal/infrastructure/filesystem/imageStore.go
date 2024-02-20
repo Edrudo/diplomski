e2fcf51453911f76f835fa136f1f13b44db300f8
eduardduras <eduard.duras@axilis.com>
2024-02-20 21:43:27 +0100

    server image storing

--- 5.semestar/diplomski/pocs/http3-poc/server/internal/infrastructure/filesystem/imageStore.go
+++ 5.semestar/diplomski/pocs/http3-poc/server/internal/infrastructure/filesystem/imageStore.go
@@ -13,14 +13,24 @@ func NewImageStore() *ImageStore {
 }
 
 func (i *ImageStore) StoreImage(imageHash string, imgBytes []byte) error {
-	out, _ := os.Create(fmt.Sprintf("/Users/eduardduras/Desktop/faks/diplomski/5.semestar/diplomski/pocs/http3-poc/server/images/%s.jpg", imageHash))
+	out, err := os.Create(
+		fmt.Sprintf(
+			// TODO change this path to be configurable, remove only .jpg extension
+			"/Users/eduardduras/Desktop/faks/diplomski/5.semestar/diplomski/pocs/http3-poc/server/images/%s.jpg",
+			imageHash,
+		),
+	)
 	defer out.Close()
+	if err != nil {
+		panic(err)
+	}
 
 	var opts jpeg.Options
 	opts.Quality = 1
 
 	// write into a file
 	if _, err := out.Write(imgBytes); err != nil {
+		// TODO add logging
 		panic(err)
 	}
 
